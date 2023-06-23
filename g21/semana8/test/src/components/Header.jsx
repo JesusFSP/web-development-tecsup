63edfbcf4964c082f18c70636c861260f2847bb7
JesusFSP <jsilva@unsa.edu.pe>
2023-06-22 19:48:45 -0500

    Agregando estilos al off-canvas

--- g21/semana8/test/src/components/Header.jsx
+++ g21/semana8/test/src/components/Header.jsx
@@ -1,13 +1,21 @@
+import { useRef } from 'react';
 import jesussilva from '../assets/img/jesussilva.jpg';
 
 const Header = ({ routeLinks, socialNetworks }) => {
+    const offCanvas = useRef();
+    const openMenu = () => {
+        offCanvas.current.classList.add('off-canvas--show')
+    }
     return (
         <header className="header">
             <nav className="nav">
                 <div className="container flexbox flexbox--header">
+                    <a href="#hero">
                     <img src={jesussilva} alt="" width={48} height={48} className="img img--logo" />
-                    <div className='off-canvas'>
-                        <ul className="list flexbox flexbox--responsive gap-4xs">
+                    </a>
+                    <div ref={offCanvas} className='off-canvas off-canvas--right '>
+                        <div className="off-canvas__child">
+                        <ul className="list flexbox flexbox--center flexbox--responsive h-100 gap-4xs">
                             {routeLinks.map((element, index) => {
                                 const { href, title, content } = element;
                                 return (
@@ -17,6 +25,7 @@ const Header = ({ routeLinks, socialNetworks }) => {
                                 );
                             })}
                         </ul>
+                        </div>
                     </div>
                     <ul className="list flexbox flexbox--responsive gap-4xs">
                         {socialNetworks.map((element, index) => {
@@ -34,6 +43,9 @@ const Header = ({ routeLinks, socialNetworks }) => {
                                 </li>
                             );
                         })}
+                        <button onClick={openMenu}>
+                            <i class="bi bi-list"></i>
+                        </button>
                     </ul>
                 </div>
             </nav>
