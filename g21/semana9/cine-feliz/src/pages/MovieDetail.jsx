61b757f46c81f7009acba05cda2a62ac26fcd239
JesusFSP <jsilva@unsa.edu.pe>
2023-06-27 22:48:42 -0500

    update g21/s9/cine-feliz get movie id

--- g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
+++ g21/semana9/cine-feliz/src/pages/MovieDetail.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const MovieDetail = () => {
+    const params = useParams();
+    console.log(params);
     return ( 
         <h1>Detalle de pelicula</h1>
      );
